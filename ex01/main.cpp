f1e5f02f604a3015a648c54039a3c4a4a8b93264
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-09-07 21:56:29 +0100

    shorten main , add constructor logic

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,17 +1,6 @@
 
 
-#include <iostream>
-#include <sstream>
-#include <stack>
-
-
-bool is_ops(std::string c)
-{
-    if (c == "+" || c == "-" || c == "/" || c == "*") 
-        return (true);
-    return (false);
-}
-
+#include "RPN.hpp"
 
 int main (int ac, char **av)
 {
@@ -20,54 +9,14 @@ int main (int ac, char **av)
         std::cerr << "Invalid input" << std::endl;
         return (1);
     }
-    std::string line = av[1];
 
-    std::istringstream iss(line);
-    std::string c;
-    std::stack<int> s;
-    int a, b, res;
-
-    while ( iss >> c)
-    {
-      
-        if (!(c.size() == 1 && std::isdigit(c[0])) && !is_ops(c))
-        {
-            std::cerr << "Invalid input" << std::endl;
-            return (1);
-        }
-
-        if (is_ops(c) && s.size() < 2)
-        {
-            std::cerr << "Error: not enough operands" << std::endl;  
-            return (1);
-        }
-        
-        if (!is_ops(c))
-            s.push(std::stoi(c));
-        else
-        { 
-            int b = s.top(); s.pop();
-            int a = s.top(); s.pop();
-            if (c == "+")       res = a + b;
-            else if (c == "-") res = a - b;
-            else if (c == "*") res = a * b;
-            else if (c == "/") {
-                if (b == 0) {
-                    std::cerr << "Error: division by zero" << std::endl;
-                    return 1;
-                }
-                res = a / b;
-            }
-            s.push(res);
-        } 
-    }
-    if (s.size() != 1)
-    {
-        std::cerr << "Error: invalid expression" << std::endl;
+    try {
+        RPN rpn(av[1]);
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
         return 1;
     }
-    else
-        std::cout << s.top() << std::endl;
+    
 
 
     return (0);
