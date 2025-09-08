31e63ab27676116749e8bf161ded5f8162bc15c3
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-09-08 16:17:36 +0100

    proper main func with try catch and PmergeMe handling

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,117 +1,22 @@
 
-#include <string>
-#include <iostream>
-#include <vector>
-#include <list>
-
-bool is_number(const char *s)
-{
-    if (!s)
-        return (false);
-    while (*s)
-    {
-        if (!std::isdigit(*s))
-            return (false);
-        ++s;
-    }
-    return (true);
-}
-
-
-void sort_vector(std::vector<int>::iterator start, std::vector<int>::iterator end)
-{
-    size_t dist = std::distance(start, end);
-    if (dist <= 1)
-        return ;
-    
-    std::vector<int>::iterator mid = start;
-    std::advance(mid, dist / 2);
-    
-    sort_vector(start, mid);
-    sort_vector(mid, end);
-    
-    std::vector<int> merged;
-    std::vector<int>::iterator left = start, right  = mid;
-
-    while (left != mid && right != end)
-    {
-        if (*left  < *right)
-            merged.push_back(*left++);
-        else
-            merged.push_back(*right++); 
-    }
-    merged.insert(merged.end(), left, mid);
-    merged.insert(merged.end(), right, end);
-    std::move(merged.begin(), merged.end(), start); 
-}
-
-void list_merge(std::list<int> &lst)
-{
-    if (lst.size() <= 1)
-        return ;
-    
-    std::list<int> left, right;
-
-    std::list<int>::iterator mid = lst.begin();
-    size_t dest = std::distance(lst.begin(), lst.end()); 
-    std::advance(mid, dest / 2);
-
-    left.splice(left.begin(), lst, lst.begin(), mid); 
-    right.splice(right.begin(), lst, mid, lst.end());
-
-    list_merge(left);
-    list_merge(right);
-
-
-    lst.merge(left);
-    lst.merge(right);
-
-}
-
+#include "PmergeMe.hpp"
 
 int main(int ac, char **av)
 {
-    // if (ac < 2)
-    // {
-    //     std::cerr << "nothing to sort" << std::endl;
-    //     return (1);
-    // }
-    // std::set<int> nbrs;
-    // for (int i = 1; i < ac; ++i)
-    // {
-    //     if (is_number(av[i]))
-    //         nbrs.insert(std::atoi(av[i]));
-    //     else
-    //     {
-    //        std::cerr << "Invalid input" << std::endl;
-    //        return (1);
-    //     }
-    // } 
-
-    // merge_set(nbrs.begin(),nbrs.end());
-
-    std::vector<int> v = {4, 3, 2,  1,5, 12,89, -6};
-    for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-        std::cout << *it << " ";
-    std::cout << std::endl;
-    
-    std::vector<int>::iterator start = v.begin(), end = v.end();
-    sort_vector(start , end);
-
-    for (std::vector<int>::iterator it = v.begin(); it != v.end(); ++it)
-        std::cout << *it << " ";
-    std::cout << std::endl;
-
-    std::cout << "========== list ============" << std::endl;
-
-    std::list<int> l = {4, 3, 2,  1,5, 12,89, -6};
-
-    list_merge(l);
-    for (std::list<int>::iterator it = l.begin(); it != l.end(); ++it)
-        std::cout << *it << " ";
-    std::cout << std::endl;
-
-
+    if (ac < 2)
+    {
+        std::cerr << "nothing to sort" << std::endl;
+        return (1);
+    }
+   
+    try {
+        PmergeMe p(av);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return (1);
+    } 
 
     return (0);
 }
